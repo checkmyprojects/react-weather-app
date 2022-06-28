1dc5a8ea5459824bbd6eb1656c8f0b3869b01b1d
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-28 20:55:58 +0200

    added footer and link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './assets/css/App.css';
 import NavBar from './components/NavBar';
 import WeatherPanel from './components/WeatherPanel';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       
     <NavBar />
     <WeatherPanel />
-
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () =>{
+    return(
+
+        <footer id="sticky-footer" class="flex-shrink-0 py-2 bg-dark text-white-50">
+            <div class="container text-center">
+                <small>Github: <a href="https://github.com/checkmyprojects/react-weather-app">checkmyprojects</a></small>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
