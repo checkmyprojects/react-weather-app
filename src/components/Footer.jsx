a10fff689ee77fe5b7bf4ccfb47008423d33d6dc
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-29 14:17:03 +0200

    added pexels link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ const Footer = () =>{
         <footer id="sticky-footer" className="flex-shrink-0 py-2 bg-dark text-white-50">
             <div className="container text-center">
                 <small>Github: <a href="https://github.com/checkmyprojects/react-weather-app">checkmyprojects</a></small>
+                <br />
+                <small>Photos provided by <a href="https://www.pexels.com">Pexels</a></small>
             </div>
         </footer>
 
